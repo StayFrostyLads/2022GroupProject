c63085a72bccc45a0ca3f19431f88e25f397aa39
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-01-06 16:48:21 -0700

    Create BackgroundPanel.java

---
+++ src/BackgroundPanel.java
@@ -0,0 +1,31 @@
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class BackgroundPanel extends JPanel {
+    JPanel game;
+    JLabel level;
+    String levelText;
+    static JButton quit, title;
+    int x;
+    
+	BackgroundPanel() {
+		setLayout(null);
+		game = new GamePage();
+		add(game);
+		x = 1;
+		levelText = "Level " + String.valueOf(x);
+		level = new JLabel(levelText);
+		level.setBounds(300,10,200,30);
+		add(level);
+		
+		quit = new JButton("Quit");
+		title = new JButton("Title Screen");
+		
+		quit.setBounds(10, 10, 100, 30);
+		title.setBounds(120, 10, 120, 30);
+		
+		add(quit);
+		add(title);
+	}
+}
