82d8b8b06bded8ba714405937486417fa2e954a4
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-01-25 10:08:53 -0700

    Update TitlePanel.java

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -1,19 +1,20 @@
+package panel;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class TitlePanel extends JPanel {
-	static JButton start, instructions, quit;
+	public static JButton start, instructions, quit;
 	JLabel title;
 	
-	TitlePanel() {
+	public TitlePanel() {
 		//setLayout(null);
 		JLabel title = new JLabel("Desert of Doom"); //Will be image in future - Stacker just say Desert of Doom in good font
 		title.setBounds(50,50,200,50);
 		add(title);
 		
 		start = new JButton("Start"); // JButtons and labels can be images, so we can design good images so it looks good
-		instructions = new JButton("Instrucitons");
+		instructions = new JButton("Instructions");
 		quit = new JButton("Quit");
 		
 		add(start);
