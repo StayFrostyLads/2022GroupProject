632c87417f7fe083997a85fce4375a4d6af7e56b
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-03-10 23:14:07 -0700

    Update DesertOfDoom.java

--- src/DesertOfDoom.java
+++ src/DesertOfDoom.java
@@ -2,20 +2,20 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.*;
 
 
-public class DesertOfDoom extends JFrame implements KeyListener {
+public class DesertOfDoom extends JFrame {
 	private JPanel cards, title, instruction, background, lose, win, gamePanel;
 	CardLayout cL = new CardLayout();
+	boolean play = false;
 	
 	public DesertOfDoom() {
 		setTitle("Desert of Doom");
 		setSize(788,716);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setFocusable(false);
 		
 		cards = new JPanel(cL);
 		title = new TitlePanel();
@@ -24,7 +24,11 @@ public class DesertOfDoom extends JFrame implements KeyListener {
 		lose = new LosePanel();
 		win = new WinPanel();
 		gamePanel = new GamePanel();
-
+		
+		cards.setFocusable(false);
+		background.setFocusable(false);
+		
+		background.add(gamePanel);
 		cards.add(title, "title");
 		cards.add(instruction, "instruction");
 		cards.add(background, "background");
@@ -33,14 +37,18 @@ public class DesertOfDoom extends JFrame implements KeyListener {
 		add(cards);
 		cL.show(cards, "title");
 		
+		revalidate();
+		
 		TitlePanel.start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cL.show(cards, "background");
+				gamePanel.requestFocusInWindow();
 			}
 		});
 		TitlePanel.instructions.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cL.show(cards, "instruction");
+				play = false;
 			}
 		});
 		TitlePanel.quit.addActionListener(new ActionListener() {
@@ -51,6 +59,7 @@ public class DesertOfDoom extends JFrame implements KeyListener {
 		InstructionPanel.start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cL.show(cards, "background");
+				gamePanel.requestFocusInWindow();
 			}
 		});
 		InstructionPanel.quit.addActionListener(new ActionListener() {
@@ -66,6 +75,7 @@ public class DesertOfDoom extends JFrame implements KeyListener {
 		BackgroundPanel.title.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				cL.show(cards,  "title");
+				play = false;
 			}
 		});
 		
@@ -77,25 +87,7 @@ public class DesertOfDoom extends JFrame implements KeyListener {
 	
 	public static void main(String[] args) {
 		DesertOfDoom game = new DesertOfDoom();
-		game.revalidate();
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
