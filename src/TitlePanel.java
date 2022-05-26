3fbb1be2627dd1e1dfbfa657b307b264d2a882f4
sophturn <70330883+sophturn@users.noreply.github.com>
2022-05-26 10:29:52 -0600

    Update TitlePanel.java

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -9,8 +9,8 @@ public class TitlePanel extends JPanel {
 	public TitlePanel() {
 		setLayout(null);
 		JLabel title = new JLabel("Desert of Doom"); //Will be image in future - Stacker just say Desert of Doom in good font
-		title.setBounds(290,50,200,50);
-		//MAKE FONT SIZE BIGGER
+		title.setBounds(100,25,700,200);
+		title.setFont(new Font("", Font.PLAIN, 80));
 		add(title);
 		
 		start = new JButton("Start"); // JButtons and labels can be images, so we can design good images so it looks good
