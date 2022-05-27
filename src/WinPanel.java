b6e6e25487b417a0e81760c65bb28a336d57fbbd
StayFrostyLads <59467621+StayFrostyLads@users.noreply.github.com>
2022-05-27 11:03:52 -0600

    Update WinPanel.java

--- src/WinPanel.java
+++ src/WinPanel.java
@@ -7,3 +7,27 @@ public class WinPanel extends JPanel {
 		add(title);
 	}
 }
+
+/*
+import java.awt.CardLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class WinPanel extends JPanel {
+	public static JButton home;
+	static CardLayout cL = new CardLayout();
+	
+	public WinPanel() {
+		JLabel title = new JLabel("YOU WIN");
+		add(title);
+		home = new JButton("Return to Title Screen");
+		WinPanel.home.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				cL.show(cards, "home");
+			}
+		});
+	}
+}
+*/
