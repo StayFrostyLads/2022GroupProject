ab71699d6ff33ee3039f30826f2c9ab632b5e682
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-05-26 21:37:04 -0600

    Update LosePanel.java

--- src/LosePanel.java
+++ src/LosePanel.java
@@ -1,5 +1,9 @@
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class LosePanel extends JPanel {
-
+	public LosePanel() {
+		JLabel title = new JLabel("YOU LOSE");
+		add(title);
+	}
 }
