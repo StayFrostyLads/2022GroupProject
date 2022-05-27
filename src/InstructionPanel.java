18adccccdb322a37b811e930a644e86691abd180
sophturn <70330883+sophturn@users.noreply.github.com>
2022-05-27 09:08:52 -0600

    Update InstructionPanel.java

--- src/InstructionPanel.java
+++ src/InstructionPanel.java
@@ -20,9 +20,11 @@ public class InstructionPanel extends JPanel {
 		start = new JButton("Start"); // JButtons and labels can be images, so we can design good images so it looks good
 		start.setBackground(Color.GREEN);
 		start.setBounds(100, 500, 200, 100);
+		start.setFont(new Font("", Font.PLAIN, 30));
 		quit = new JButton("Quit");
 		quit.setBackground(Color.RED);
 		quit.setBounds(450, 500, 200, 100);
+		quit.setFont(new Font("", Font.PLAIN, 30));
 		
 		setBackground(Color.ORANGE);
 		add(start);
