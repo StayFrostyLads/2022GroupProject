9e41d9727b3fc32f6fae0e59faaa5bdebd495404
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-05-26 21:36:24 -0600

    Update WinPanel.java

--- src/WinPanel.java
+++ src/WinPanel.java
@@ -1,5 +1,9 @@
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class WinPanel extends JPanel {
-
+	public WinPanel() {
+		JLabel title = new JLabel("YOU WIN");
+		add(title);
+	}
 }
