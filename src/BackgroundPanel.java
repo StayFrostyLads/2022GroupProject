2f613ede25538f9608ba611af9f24daa4d4f0256
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-05-29 17:58:27 -0600

    Update BackgroundPanel.java

--- src/BackgroundPanel.java
+++ src/BackgroundPanel.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class BackgroundPanel extends JPanel {
     JPanel game;
@@ -13,22 +14,39 @@ public class BackgroundPanel extends JPanel {
     
 	public BackgroundPanel() {
 		setLayout(null);
+		Color brown = new Color(194, 178, 128);
+		Color color = new Color(152, 133, 88);
+		setBackground(color);
 		points = 0;
 		levelText = "Level: ";
 		level = new JLabel(levelText, JLabel.CENTER);
+		level.setForeground(Color.BLACK);
 		level.setFont(new Font("Garamond", Font.PLAIN, 30));
-		level.setBounds(380,10,200,30);
+		level.setBounds(280,20,200,30);
 		point = new JLabel("Score: " + points);
 		point.setFont(new Font("Garamond", Font.PLAIN, 30));
-		point.setBounds(600,10,200,30);
+		point.setBounds(500,20,200,30);
+		point.setForeground(Color.BLACK);
 		add(level);
 		add(point);
 		
 		quit = new JButton("Quit");
 		title = new JButton("Title Screen");
 		
-		quit.setBounds(10, 10, 100, 30);
-		title.setBounds(120, 10, 120, 30);
+		quit.setBounds(140, 20, 100, 30);
+		title.setBounds(10, 20, 120, 30);
+		
+		quit.setBackground(brown);
+		title.setBackground(brown);
+		
+		quit.setForeground(Color.BLACK);
+		title.setForeground(Color.BLACK);
+		
+		quit.setFont(new Font("Garamond", Font.PLAIN, 17));
+		title.setFont(new Font("Garamond", Font.PLAIN, 17));
+		
+		quit.setBorder(new LineBorder(Color.BLACK));
+		title.setBorder(new LineBorder(Color.BLACK));
 		
 		add(quit);
 		add(title);
