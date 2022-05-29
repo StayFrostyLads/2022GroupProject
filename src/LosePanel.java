7cf7beab5cbb4b0c95b1e6295174614c8cf16c21
sophturn <70330883+sophturn@users.noreply.github.com>
2022-05-28 23:40:15 -0600

    Update LosePanel.java

--- src/LosePanel.java
+++ src/LosePanel.java
@@ -1,9 +1,14 @@
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.*;
 
 public class LosePanel extends JPanel {
 	public LosePanel() {
+		setLayout(null);
 		JLabel title = new JLabel("YOU LOSE");
+		title.setBounds(160,200,600,200);
+		title.setFont(new Font("Garamond", Font.PLAIN, 90));;
 		add(title);
+		setBackground(Color.ORANGE);
 	}
 }
