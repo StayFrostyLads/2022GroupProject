ffa75bd13e851f1f0da74d09b24d04efc80a0830
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-05-29 18:02:40 -0600

    Update WinPanel.java

--- src/WinPanel.java
+++ src/WinPanel.java
@@ -1,38 +1,45 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.*;
+import javax.swing.border.LineBorder;
 
 public class WinPanel extends JPanel {
+	public static JButton quit, titleB;
+	Color brown = new Color(194, 178, 128);
 	public WinPanel() {
-		setBackground(Color.ORANGE);
 		setLayout(null);
-		JLabel title = new JLabel("YOU WIN");
-		title.setBounds(160,200,600,200);
-		title.setFont(new Font("Garamond", Font.PLAIN, 90));
+		Color color = new Color(152, 133, 88);
+		setBackground(color);
+		URL urlW = DesertOfDoom.class.getResource("/images/you win.png");
+		ImageIcon w = new ImageIcon(urlW);
+		JLabel title = new JLabel(w);
+		title.setBounds(30,180,700,300);
 		add(title);
+		
+		quit = new JButton("Quit");
+		titleB = new JButton("Title Screen");
+		
+		quit.setBounds(140, 20, 100, 30);
+		titleB.setBounds(10, 20, 120, 30);
+		
+		quit.setBackground(brown);
+		titleB.setBackground(brown);
+		
+		quit.setForeground(Color.BLACK);
+		titleB.setForeground(Color.BLACK);
+		
+		quit.setFont(new Font("Garamond", Font.PLAIN, 17));
+		titleB.setFont(new Font("Garamond", Font.PLAIN, 17));
+		
+		quit.setBorder(new LineBorder(Color.BLACK));
+		titleB.setBorder(new LineBorder(Color.BLACK));
+		
+		add(quit);
+		add(titleB);
 	}
 }
-
-/*
-import java.awt.CardLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.*;
-
-public class WinPanel extends JPanel {
-	public static JButton home;
-	static CardLayout cL = new CardLayout();
-	
-	public WinPanel() {
-		JLabel title = new JLabel("YOU WIN");
-		add(title);
-		home = new JButton("Return to Title Screen");
-		WinPanel.home.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				cL.show(cards, "home");
-			}
-		});
-	}
-}
-*/
