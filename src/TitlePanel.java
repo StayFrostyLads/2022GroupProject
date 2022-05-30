9aec41bfe294eacf8f1dd3bb4fa30722eeedc289
HenryChristiansen <61328987+HenryChristiansen@users.noreply.github.com>
2022-05-29 18:02:15 -0600

    Update TitlePanel.java

--- src/TitlePanel.java
+++ src/TitlePanel.java
@@ -1,32 +1,44 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.net.URL;
 
 public class TitlePanel extends JPanel {
 	public static JButton start, instructions, quit;
 	JLabel title;
 	
 	public TitlePanel() {
+		Color brown = new Color(194, 178, 128);
+		URL urlT = DesertOfDoom.class.getResource("/images/title.png");
+		ImageIcon t = new ImageIcon(urlT);
+		JLabel title = new JLabel(t);
 		setLayout(null);
-		JLabel title = new JLabel("Desert of Doom"); //Will be image in future - Stacker just say Desert of Doom in good font
-		title.setBounds(100,25,700,200);
-		title.setFont(new Font("Garamond", Font.PLAIN, 80));
+		title.setBounds(20,25,750,200);
 		add(title);
 		
-		start = new JButton("Start"); // JButtons and labels can be images, so we can design good images so it looks good
-		start.setBackground(Color.GREEN);
+		start = new JButton("Start"); 
+		start.setBackground(brown);
 		start.setBounds(290,250,200,250);
 		start.setFont(new Font("Garamond", Font.PLAIN, 30));
+		start.setBorder(new LineBorder(Color.BLACK));
+		start.setForeground(Color.BLACK);
 		instructions = new JButton("Instructions");
-		instructions.setBackground(Color.CYAN);
+		instructions.setForeground(Color.BLACK);
+		instructions.setBackground(brown);
 		instructions.setBounds(40,250,200,250);
 		instructions.setFont(new Font("Garamond", Font.PLAIN, 30));
+		instructions.setBorder(new LineBorder(Color.BLACK));
 		quit = new JButton("Quit");
-		quit.setBackground(Color.RED);
+		quit.setBackground(brown);
+		quit.setForeground(Color.BLACK);
+		quit.setBorder(new LineBorder(Color.BLACK));
 		quit.setBounds(540,250,200,250);
 		quit.setFont(new Font("Garamond", Font.PLAIN, 30));
 		
-		setBackground(Color.ORANGE);
+		Color color = new Color(152, 133, 88);
+		setBackground(color);
 		add(start);
 		add(quit);
 		add(instructions);
